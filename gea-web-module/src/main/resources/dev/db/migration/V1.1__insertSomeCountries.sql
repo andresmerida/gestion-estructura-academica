836607cfa276bc6b0db93a21b3d6e6c9397de793
Andres <andresmerida1@gmail.com>
2021-11-30 07:54:38 -0400

    JIRA-02 dev connection db multiprofile

---
+++ gea-web-module/src/main/resources/dev/db/migration/V1.1__insertSomeCountries.sql
@@ -0,0 +1,1 @@
+INSERT INTO country (name, initials,active) values('Bolivia', 'BOL', true),('Peru', 'PER', true);
---
+++ gea-web-module/src/main/resources/test/db/migration/V1.1__insertSomeCountriesTestEnv.sql
@@ -0,0 +1,3 @@
+INSERT INTO country (name, initials,active) values('Bolivia', 'BOL', true),('Peru', 'PER', true),
+                                                  ('Chile', 'CHI', true),('Paraguay', 'PAR', true),
+                                                  ('Brasil', 'BRA', true),('Argentina', 'ARG', true);
